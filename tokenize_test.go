3022aeb8f09a7c2f882d6ed7fb1046b21e0eddff
Jacob Moody <jsmoody@iastate.edu>
2019-08-10 21:01:47 -0500

    Change fmt verbs for t.Fatal

--- tokenize_test.go
+++ tokenize_test.go
@@ -11,20 +11,20 @@ func TestTokenize(t *testing.T) {
 	expected = []string{`Hello`, `World`}
 	fields := Tokenize(arg)
 	if fields[0] != expected[0] || fields[1] != expected[1] {
-		t.Fatalf("Expected %s got %v", expected, fields)
+		t.Fatalf("Expected %v got %v", expected, fields)
 	}
 
 	arg = `test 'test 123'`
 	expected = []string{`test`, `test 123`}
 	fields = Tokenize(arg)
 	if fields[0] != expected[0] || fields[1] != expected[1] {
-		t.Fatalf("Expected %s got %v", expected, fields)
+		t.Fatalf("Expected %v got %v", expected, fields)
 	}
 
 	arg = `test 'test ''123'`
 	expected = []string{`test`, `test '123`}
 	fields = Tokenize(arg)
 	if fields[0] != expected[0] || fields[1] != expected[1] {
-		t.Fatalf("Expected %s got %v", expected, fields)
+		t.Fatalf("Expected %v got %v", expected, fields)
 	}
 }
